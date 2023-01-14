4b3356c453e71f36be83eac80aeb71ee43b29992
Ignacio Moscatelli <inm300300@gmail.com>
2023-01-13 23:56:48 -0300

    Agrego Login, productCart, productDetail, register y sus respectivos header y footer (mobile solo)

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3009;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/index.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto",PORT);
+});
