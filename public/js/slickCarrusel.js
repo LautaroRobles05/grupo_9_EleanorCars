b0cc88d62df89e76ecab9fb964847fca8f3c76e8
LautaroRobles05 <lautaro.robles1992@gmail.com>
2023-05-18 21:51:50 -0300

    modificando estilos y animaciones

--- public/js/slickCarrusel.js
+++ public/js/slickCarrusel.js
@@ -6,4 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
         fade: true,
         cssEase: 'linear'
       });
+
+
+     
 })
+
+let imagen = document.querySelector('.container_img_top');
+
+imagen.addEventListener('mousedown', function() {
+  image.style.cursor = 'grab';
+});
+
+imagen.addEventListener('mouseleave', function() {
+  document.body.style.cursor = 'default';
+});
+
+let imagen2 = document.querySelector('.container_img');
+
+imagen2.addEventListener('mousedown', function() {
+  imagen2.style.cursor = 'grab';
+});
+
+imagen2.addEventListener('mouseleave', function() {
+  imagen2.style.cursor = 'default';
+});
