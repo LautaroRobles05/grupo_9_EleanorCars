bd9673ad55a12c369cb989cca1505eba596c77c0
LautaroRobles05 <lautaro.robles1992@gmail.com>
2023-05-20 14:01:13 -0300

    dropdown mobile user y dropdown burguer menu mobile

--- public/js/dropdownMenu.js
+++ public/js/dropdownMenu.js
@@ -1,5 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  //DROPDOWN BURGUER MENU
+  let subMenuBurguerMobile = document.getElementById('subMenuBurguerMobile')
+  let profileBurguerDropdownMobile = document.querySelector('.burguer-menu')
+  console.log('HOLAAAA', profileBurguerDropdownMobile)  
+  profileBurguerDropdownMobile.addEventListener('click', () => {
+    subMenuBurguerMobile.classList.toggle("open-menu")
+    subMenuMobile.classList.remove("open-menu")
+    
+  })
+
+  //DROPDOWN PROFILE MOBILE
+  let subMenuMobile = document.getElementById('subMenuMobile')
+  let profileDropdownMobile = document.querySelector('.profile-menu')
+  console.log(profileDropdownMobile)  
+  profileDropdownMobile.addEventListener('click', () => {
+    subMenuMobile.classList.toggle("open-menu")
+    subMenuBurguerMobile.classList.remove("open-menu")
+
+  })
+  
+  
+
+  //DROPDOWN HIGH RESOLUTIONS
   let subMenu = document.getElementById('subMenu')
   let profileDropdown = document.getElementById('profile-dropdown')
   
